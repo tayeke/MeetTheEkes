1cdafdc954fc2808bf380a11b52b19359f098c24
Taylor Eke <tayloreke@gmail.com>
2019-02-12 22:26:23 -0800

    ferry links and marker manager

--- src/map.js
+++ src/map.js
@@ -9,6 +9,7 @@ var map,
             <div class="center-text">
                 <h3 class="two-em blue">Ferry Terminal:</h3>
                 <p class="minion one-five-em blue">The only way to reach Orcas Island is by ferry, via Anacortes if you’re coming from Portland or Seattle.</p>
+                <p><a href="https://secureapps.wsdot.wa.gov/ferries/reservations/Vehicle/SailingSchedule.aspx" target="_blank">Washington State Ferry Reservations</a></p>
             </div>
             `,
             latlng: {lat: 48.51, lng: -122.6828207}
@@ -105,6 +106,14 @@ function initMap() {
       // styles: [{"featureType":"all","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"all","elementType":"labels","stylers":[{"visibility":"off"},{"saturation":"-100"}]},{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40},{"visibility":"off"}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"off"},{"color":"#000000"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"landscape","elementType":"geometry.fill","stylers":[{"color":"#4d6059"}]},{"featureType":"landscape","elementType":"geometry.stroke","stylers":[{"color":"#4d6059"}]},{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"color":"#4d6059"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"lightness":21}]},{"featureType":"poi","elementType":"geometry.fill","stylers":[{"color":"#4d6059"}]},{"featureType":"poi","elementType":"geometry.stroke","stylers":[{"color":"#4d6059"}]},{"featureType":"road","elementType":"geometry","stylers":[{"visibility":"on"},{"color":"#7f8d89"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"color":"#7f8d89"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#7f8d89"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#7f8d89"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#7f8d89"}]},{"featureType":"road.arterial","elementType":"geometry.stroke","stylers":[{"color":"#7f8d89"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"road.local","elementType":"geometry.fill","stylers":[{"color":"#7f8d89"}]},{"featureType":"road.local","elementType":"geometry.stroke","stylers":[{"color":"#7f8d89"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#2b3638"},{"visibility":"on"}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#2b3638"},{"lightness":17}]},{"featureType":"water","elementType":"geometry.fill","stylers":[{"color":"#24282b"}]},{"featureType":"water","elementType":"geometry.stroke","stylers":[{"color":"#24282b"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels.text.stroke","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels.icon","stylers":[{"visibility":"off"}]}]
     });
 
+    map.addListener("click", function(event) {
+        for (var i = 0; i < icons.length; i++ ) {
+            if(typeof icons[i].infoWindow !== 'undefined') {
+                icons[i].infoWindow.close();
+            }
+        }
+    });
+
     function animateIn(marker) {
       marker.setAnimation(google.maps.Animation.BOUNCE);
     }
@@ -112,12 +121,18 @@ function initMap() {
       marker.setAnimation(null);
     }
 
-    function openMarker(marker, content) {
-      var infoWindow = new google.maps.InfoWindow({
-          content: content,
-          maxWidth: 200
-      });
-      infoWindow.open(map, marker);
+    function openMarker(icon) {
+      if(typeof icon.infoWindow === 'undefined') {
+          var infoWindow = new google.maps.InfoWindow({
+              content: icon.content,
+              maxWidth: 200
+          });
+          infoWindow.addListener("click", function(event) {
+              event.stopPropagation();
+          });
+          icon.infoWindow = infoWindow;
+      }
+      icon.infoWindow.open(map, icon.marker);
     }
 
     for(var i = 0; i < icons.length; i++) {
@@ -135,7 +150,7 @@ function initMap() {
       });
       icons[i].marker.addListener('mouseover', animateIn.bind(null, icons[i].marker));
       icons[i].marker.addListener('mouseout', animateOut.bind(null, icons[i].marker));
-      icons[i].marker.addListener('click', openMarker.bind(null, icons[i].marker, icons[i].content));
+      icons[i].marker.addListener('click', openMarker.bind(null, icons[i]));
     }
 
   });
